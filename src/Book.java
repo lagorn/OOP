734980f1f808eef5e2b960ccae8cec67bbd2d2d9
lagorn <lagorn@ro.ru>
2019-07-15 17:04:33 +0400

    add class book

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,52 @@
+import java.util.Objects;
+
 public class Book {
+    private String author;
+    private String title;
+    private int age;
+
+    public Book(String author, String title, int age) {
+        this.author = author;
+        this.title = title;
+        this.age = age;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+        Book book = (Book) o;
+        return getAge() == book.getAge() &&
+                Objects.equals(getAuthor(), book.getAuthor()) &&
+                Objects.equals(getTitle(), book.getTitle());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getAuthor(), getTitle(), getAge());
+    }
 }
