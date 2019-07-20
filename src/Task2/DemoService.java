63a67c1ea29884cebb71af4ed77d5568895595a5
lagorn <lagorn@ro.ru>
2019-07-20 18:08:40 +0400

    create DemoServise

--- src/Task2/DemoService.java
+++ src/Task2/DemoService.java
@@ -1,4 +1,9 @@
 package Task2;
 
 public class DemoService {
+    public void execute(){
+        System.out.println("Создаем 5 студентов");
+
+        Student student1 = new Student("Ivanov I.I.",1,StudentProgress(int[] marks{2,3,5,4,3};))
+    }
 }
